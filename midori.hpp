0af984fce0c1669f8b5fdd376cc715fc8324833c
0xIce <92980874+IceIsHere@users.noreply.github.com>
2022-03-26 00:57:40 +0430

    Initial commit

---
+++ midori.hpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+
+template <typename Iterator, typename Sentinel = Iterator> struct range_t {
+
+  Iterator beg;
+  Sentinel e;
+  Iterator begin() const { return beg; }
+  Sentinel end() const { return e; }
+
+  bool empty() const { return begin() == end(); }
+
+  range_t without_front(std::size_t count = 1) const {
+    if constexpr (std::is_same<std::random_access_iterator_tag,
+                               typename std::iterator_traits<
+                                   Iterator>::iterator_category>{}) {
+
+      count = (std::min)(std::size_t(std::distance(beg, e)), count);
+    }
+
+    return {std::next(beg, count), e};
+  }
+
+  range_t without_back(std::size_t count = 1) const {
+    if constexpr (std::is_same<std::random_access_iterator_tag,
+                               typename std::iterator_traits<
+                                   Iterator>::iterator_category>{}) {
+
+      count = (std::min)(std::size_t(std::distance(beg, e)), count);
+    }
+
+    return {beg, std::prev(e, count)};
+  }
+};
+
+template <typename Iterator, typename Sentinel>
+range_t<Iterator, Sentinel> range(Iterator beg, Sentinel e) {
+  return {beg, e};
+}
+
+template <typename Iterable> auto range(Iterable &r) {
+  return range(std::begin(r), std::end(r));
+}
+
+template <typename C> constexpr auto except_first(C &c) {
+  auto r = range(c);
+  if (r.empty())
+    return r;
+
+  return r.without_front();
+}
+
+template <typename C> constexpr auto except_last(C &c) {
+  auto r = range(c);
+  if (r.empty())
+    return r;
+
+  return r.without_back();
+}
+
+// Only works with std::vector and std::deque
+constexpr auto except_index(auto &container,
+                            std::initializer_list<std::size_t> index) {
+
+  if (container.empty())
+    return container;
+  auto arr = container;
+
+  for (auto i : index) {
+    arr.erase(std::remove(arr.begin(), arr.end(), arr.at(i)));
+  }
+
+  return arr;
+}
