52105e2cca0c4b691a0aac51ce40460b6a766551
0xIce <92980874+IceIsHere@users.noreply.github.com>
2022-03-26 12:06:38 +0430

    from

--- midori.hpp
+++ midori.hpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <numeric>
+#include <vector>
 
 template <typename Iterator, typename Sentinel = Iterator> struct range_t {
 
@@ -57,17 +59,28 @@ template <typename C> constexpr auto except_last(C &c) {
   return r.without_back();
 }
 
-// Only works with std::vector and std::deque
+// only works with std::vector and std::deque
 constexpr auto except_index(auto &container,
                             std::initializer_list<std::size_t> index) {
 
   if (container.empty())
     return container;
+
   auto arr = container;
+  std::size_t count{0};
 
   for (auto i : index) {
-    arr.erase(std::remove(arr.begin(), arr.end(), arr.at(i)));
+    arr.erase(std::remove(arr.begin(), arr.end(), arr.at(i - count)));
+
+    count++;
   }
 
   return arr;
+}
+
+auto from(int one, int two) {
+  std::vector<int> vec(two);
+  std::iota(vec.begin(), vec.end(), one);
+
+  return vec;
 }
